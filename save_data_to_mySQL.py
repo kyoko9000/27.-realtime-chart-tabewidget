ae5c48ea8631356ffab54739a7af86e73ead73fd
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-08-18 18:29:08 +0700

    Update save_data_to_mySQL.py

--- save_data_to_mySQL.py
+++ save_data_to_mySQL.py
@@ -1,13 +1,16 @@
 import mysql.connector
 
+
 class save_data_handle():
+
     def __init__(self, row_po=0, row_val=0, column=0):
         super(save_data_handle, self).__init__()
+        print("life", row_po, row_val, column)
 
         db = mysql.connector.connect(user='root', password='1234',
                                      host='127.0.0.1', database="new_database")
 
-        if column == 1:
+        if column == 2:
             sql = "UPDATE `distance` SET `name`= %s WHERE `ID` = %s"
             val = (row_val, row_po)
             mycursor = db.cursor()  # run command
@@ -15,10 +18,11 @@ class save_data_handle():
             db.commit()
             print(mycursor.rowcount, "record(s) affected")
 
-        elif column == 2:
+        elif column == 3:
             sql = "UPDATE `distance` SET `km`= %s WHERE `ID` = %s"
             val = (row_val, row_po)
             mycursor = db.cursor()  # run command
             mycursor.execute(sql, val)
             db.commit()
             print(mycursor.rowcount, "record(s) affected")
+
