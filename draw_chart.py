94e217705210faacbbf02f291456a576be8abfcb
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-08-18 18:29:04 +0700

    Update draw_chart.py

--- draw_chart.py
+++ draw_chart.py
@@ -6,6 +6,7 @@ import mysql.connector as load_mySQL
 
 timer = QTimer()
 
+
 class show_chart(FigureCanvasQTAgg):
     def __init__(self):
         self.fig, self.ax = plt.subplots()
@@ -15,9 +16,10 @@ class show_chart(FigureCanvasQTAgg):
         timer.start(2000)
 
         plt.ion()
+
     def loop(self):
         db = load_mySQL.connect(user='root', password='1234',
-                                     host='127.0.0.1', database='new_database')
+                                host='127.0.0.1', database='new_database')
         # lenh chay
         code_8 = 'SELECT name,km FROM distance'
         # lệnh chạy code
@@ -33,4 +35,4 @@ class show_chart(FigureCanvasQTAgg):
 
         self.ax.clear()
         self.ax.pie(datas1, labels=datas, autopct='%1.2f%%', explode=explode,
-               shadow=True, startangle=90)  # , explode=explode
+                    shadow=True, startangle=90)  # , explode=explode
